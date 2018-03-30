1d123d21ce15e0f2056f9f547e488a34c667902b
Matt Patrick <hazmat345@gmail.com>
2018-03-30 14:40:06 -0400

    Adding jest dependency and configuration

---
+++ brew_view/static/test/setup.js
@@ -0,0 +1,12 @@
+//import "angular";
+//import "angular-mocks";
+
+import chai from "chai";
+import sinon from "sinon";
+import sinonChai from "sinon-chai";
+
+chai.use(sinonChai);
+
+global.jestExpect = global.expect;
+global.expect = chai.expect;
+global.sinon = sinon;
